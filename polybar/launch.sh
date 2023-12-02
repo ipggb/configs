88528498b6d8909c7b24d8618899691d5b50bc0a
ipggb <isor.gh9@gmail.com>
2023-12-03 00:35:05 +0545

    Added polybar configs

---
+++ polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar bspwm-bar 2>&1 | tee -a /tmp/polybar1.log & disown
+
+echo "Bars launched..."
+
